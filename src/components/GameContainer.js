2faf39f578ae69b0df321415f17816ad3efe09bf
tallybee <talbondareva@gmail.com>
2019-07-28 11:08:35 +0200

    first logic solution

--- src/components/GameContainer.js
+++ src/components/GameContainer.js
@@ -4,7 +4,24 @@ import Game from './Game'
 
 
 class GameContainer extends React.Component {
+  state = {
+    cardsInOrder: [1,2,3,4]
+  }
+  handleClick = event => {
 
+    if (this.state.cardsInOrder.length > 0) {
+      const firstCard = this.state.cardsInOrder.shift();
+      if (firstCard === event && this.state.cardsInOrder.length > 0) {
+        console.log('doing well')
+      } else if (firstCard === event && this.state.cardsInOrder.length === 0) {
+        console.log('you won')
+      } else {
+        console.log('you lost')
+      }
+    } else {
+      console.log('boo')
+    }
+  };
   
   render() {
     return <Game cards={this.props.cards} handleClick={this.handleClick}/>
