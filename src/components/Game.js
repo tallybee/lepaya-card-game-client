6edd7dfba46c53464c788cdd4d03b916f72f7da7
tallybee <talbondareva@gmail.com>
2019-07-28 14:31:08 +0200

    cards hidden

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,14 +1,20 @@
 import React from 'react'
 
 export default function Game(props) {
-  
   return (
     <div>
-      <button key='1'onClick={() => props.handleClick(1)}>'1'</button>
-      <button key='2'onClick={() => props.handleClick(2)}>'2'</button>
-      <button key='3'onClick={() => props.handleClick(3)}>'3'</button>
-      <button key='4'onClick={() => props.handleClick(4)}>'4'</button>
-  </div>
+      <button key="1" onClick={() => props.handleClick(1)}>
+        {props.hideCards ? ' ' : ' 1 '}
+      </button>
+      <button key="2" onClick={() => props.handleClick(2)}>
+      {props.hideCards ? ' ' : ' 2 '}
+      </button>
+      <button key="3" onClick={() => props.handleClick(3)}>
+      {props.hideCards ? ' ' : ' 3 '}
+      </button>
+      <button key="4" onClick={() => props.handleClick(4)}>
+      {props.hideCards ? ' ' : ' 4 '}
+      </button>
+    </div>
   )
 }
-
--- src/components/GameContainer.js
+++ src/components/GameContainer.js
@@ -1,37 +1,57 @@
 import React from 'react'
-import {connect} from 'react-redux'
+import { connect } from 'react-redux'
 import Game from './Game'
-
 
 class GameContainer extends React.Component {
   state = {
-    cardsInOrder: [1,2,3,4]
+    cardsInOrder: [1, 2, 3, 4],
+    hideCards: false
   }
-  handleClick = event => {
 
+  componentDidMount() {
+    this.flipCards()
+  }
+
+  flipCards() {
+    setTimeout(() => {
+      this.setState({
+        hideCards: true
+      })
+    }, 3000)
+  }
+
+  handleClick = event => {
     if (this.state.cardsInOrder.length > 0) {
-      const firstCard = this.state.cardsInOrder.shift();
+      const firstCard = this.state.cardsInOrder.shift()
       if (firstCard === event && this.state.cardsInOrder.length > 0) {
         console.log('doing well')
       } else if (firstCard === event && this.state.cardsInOrder.length === 0) {
-        console.log('you won')
+        alert('you won')
       } else {
-        console.log('you lost')
+        alert('you lost')
       }
     } else {
-      console.log('boo')
+      alert('you lost')
     }
-  };
-  
+  }
+
   render() {
-    return <Game cards={this.props.cards} handleClick={this.handleClick}/>
+    return (
+      <Game
+        cards={this.props.cards}
+        hideCards={this.state.hideCards}
+        handleClick={this.handleClick}
+      />
+    )
   }
 }
 
 const mapStateToProps = state => ({
-  cards: state.cards
+  cards: state.cards,
+  active: state.active
 })
 
-export default connect(mapStateToProps
+export default connect(
+  mapStateToProps
   // ,   {loadCards}
-  )(GameContainer)
+)(GameContainer)
